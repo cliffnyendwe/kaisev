139a119c53c141e64297427cf5f5ad322004701f
Maskoff0 <mutukusamson049@gmail.com>
2026-08-29 22:47:40 +0300

    Edits

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,6 +73,8 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
+# Database
+# https://djangoproject.com
 
 DATABASES = {
     'default': {
